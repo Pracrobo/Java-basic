4fb4876ae4f1941d6885c5e534c3536ee5f01c5e
Pracrobo <cpu180621@gmail.com>
2024-01-03 10:58:07 +0900

    240103 상속 이해하기 1

---
+++ src/extends1/ex2/Car.java
@@ -0,0 +1,7 @@
+package extends1.ex2;
+
+public class Car { //슈퍼 클래스
+    public void move() {
+        System.out.println("차 이동합니다.");
+    }
+}
---
+++ src/extends1/ex2/GasCar.java
@@ -0,0 +1,8 @@
+package extends1.ex2;
+
+public class GasCar extends Car{ // 서브 클래스
+    public void fillUp() {
+        System.out.println("기름 주유합니다.");
+    }
+
+}
